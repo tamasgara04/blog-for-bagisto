855f188c512235fdebab5ddb5b0b226c7bc5cb70
tamasgara04 <ttamas104@gmail.com>
2024-06-04 17:07:06 +0200

    fix

--- src/Database/Migrations/2024_05_31_00000_create_blog_category_translations.php
+++ src/Database/Migrations/2024_05_31_00000_create_blog_category_translations.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBlogCategoriesTable extends Migration
+class CreateBlogCategoriesTranslationTable extends Migration
 {
     public function up()
     {
