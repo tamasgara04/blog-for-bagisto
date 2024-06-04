e0f7b122692ce890af6fa5983990d099d09be96e
tamasgara04 <ttamas104@gmail.com>
2024-06-04 17:15:58 +0200

    migration name fixes

--- src/Database/Migrations/2024_05_31_00000_update_blog_category.php
+++ src/Database/Migrations/2024_05_31_00000_update_blog_category.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateBlogCategoriesTable extends Migration
+class UpdateBlogCategory extends Migration
 {
     public function up()
     {
